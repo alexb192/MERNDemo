7651fc7af4e91aa742414cef648e5b88b774e2d3
alexb192 <alex.birtwistle4@gmail.com>
2020-09-08 15:46:34 -0400

    pushing to github

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
 // use routes
 app.use("/api/items", items);
 
-// Serve static assits if in production
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
