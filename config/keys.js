303421a3ec1d186a8249e31c632b55b6bb774b0a
Alex <37848326+alexb192@users.noreply.github.com>
2020-09-08 16:10:09 -0400

    Update keys.js
    exposed mongo uri

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://alexb192:arrowhead@cluster0.rzob5.mongodb.net/MernShopping?retryWrites=true&w=majority'
+    mongoURI: ''
 }
+
+// hiding my mongodb uri!
